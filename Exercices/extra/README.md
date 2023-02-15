1d48d02b5d8690a49c19b89e7c7899544d57d0f0
ubajack <jacques.bach@ubalia.fr>
2023-02-15 10:04:12 +0100

    Extra exercise

---
+++ Exercices/extra/README.md
@@ -0,0 +1,6 @@
+# Extra
+
+- Lancez live-server ou ouvrez le document `extra.html` dans votre navigateur
+- Sur la base de l'image ci-dessous (et à l'aide des liens https://youtu.be/Urwzk6ILvPQ et https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#array_methods_and_empty_slots), appliquez les fonctions dans le fichier `extra.js` pour obtenir le résultat escompté :
+
+![](20230213_014657.jpg)
---
+++ Exercices/extra/extra.js
@@ -0,0 +1,18 @@
+/* Dans le fichier extra-prep
+const toMap = ['🍔', '🍔', '🍔'];
+const toFilter = ['🍟', '🍔', '🍟'];
+const toFind = ['🍟', '🍔', '🍟'];
+const toFindWithIndex = ['🍟', '🍔', '🍟'];
+const toFill = ['🍔', '🍔', '🍔'];
+const doSome = ['🍟', '🍔', '🍟'];
+const doEvery = ['🍟', '🍔', '🍟'];
+*/
+
+// TODO: appliquez les fonctions ici
+const toMapResult = [];
+const toFilterResult = [];
+const toFindResult = [];
+const toFindWithIndexResult = -1;
+const toFillResult = [];
+const doSomeResult = false;
+const doEveryResult = false;
