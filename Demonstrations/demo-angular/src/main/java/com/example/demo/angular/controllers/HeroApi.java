384c332f2500e986572002dd198647a93eb38e2f
ubajack <jacques.bach@ubalia.fr>
2023-02-17 09:42:55 +0100

    Add some prints

--- Demonstrations/demo-angular/src/main/java/com/example/demo/angular/controllers/HeroApi.java
+++ Demonstrations/demo-angular/src/main/java/com/example/demo/angular/controllers/HeroApi.java
@@ -18,26 +18,31 @@ public class HeroApi {
 
     @GetMapping("/heroes")
     public List<Hero> all() {
+        System.out.println("Fetched all heroes");
         return heroServiceInterface.getAll();
     }
 
     @GetMapping("/heroes/{id}")
     public Hero one(@PathVariable Long id) {
+        System.out.println("Fetched hero with id: " + id);
         return heroServiceInterface.getOne(id);
     }
 
     @PostMapping("/heroes")
     public Hero add(@RequestBody Hero hero) {
+        System.out.println("Got a hero");
         return heroServiceInterface.add(hero);
     }
 
     @PutMapping("/heroes")
     public Hero update(@RequestBody Hero hero) {
+        System.out.println("Updated a hero");
         return heroServiceInterface.update(hero);
     }
 
     @DeleteMapping("/heroes/{id}")
     public void delete(@PathVariable Long id) {
+        System.out.println("Deleted hero with id: " + id);
         heroServiceInterface.deleteById(id);
     }
 
